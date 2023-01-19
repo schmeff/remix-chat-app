8ff1a7024a00fc818ddd550e3eccd158ce12a6a1
Jeff <jcelsey@gmail.com>
2023-01-19 15:11:54 -0700

    Got loading messages from db working with broadcasting messages

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,5 @@
-import { Form, useActionData, useLoaderData } from '@remix-run/react';
-import { useEffect, useState } from 'react';
+import { Form, useActionData, useFetcher, useLoaderData } from '@remix-run/react';
+import { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client'
 import { db } from '../util/db.server'
 import { type ActionArgs } from '@remix-run/node';
@@ -13,8 +13,6 @@ interface Message {
 }
 
 export async function loader() {
-
-
   return await db.message.findMany();
 }
 
@@ -33,16 +31,18 @@ export async function action({ request }: ActionArgs) {
       author
     }
   });
-  socket.emit("message", message);
-  return message;
+  return message
 }
 
-function useMessagingListener() {
-  const [messages, setMessages] = useState<string[]>([]);
+export default function Index() {
+  const [messages, setMessages] = useState(useLoaderData<typeof loader>() as Message[])
+  const receivedMessage = useActionData<typeof action>() as Message
+
+  const formRef = useRef<HTMLFormElement>(null);
 
   useEffect(() => {
-    socket.on('receiveMessage', (receivedMessage: string) => {
-      setMessages((prevMessages: string[]) => [...prevMessages, receivedMessage])
+    socket.on('receiveMessage', (receivedMessage: Message) => {
+      setMessages(prevMessages => [...prevMessages, receivedMessage]);
     })
 
     return () => {
@@ -50,22 +50,23 @@ function useMessagingListener() {
     }
   }, []);
 
-  return [messages, setMessages] as const;
-}
-
-export default function Index() {
-  const messages: Message[] = useLoaderData<typeof loader>() as Message[]
-  const [messageInput, setMessageInput] = useState('');
+  useEffect(() => {
+    if (receivedMessage) {
+      formRef.current?.reset();
+      socket.emit('message', receivedMessage)
+    }
+  }, [receivedMessage])
 
   return (
     <div className='w-1/2 mx-auto h-screen grid place-content-center'>
-      <div className='chat-container border-2 rounded-md border-stone-800 p-2 h-96'>
+      <div className='chat-container border-2 rounded-md border-stone-800 p-2 h-96 overflow-y-scroll'>
         {messages.map((message: Message) => <div key={message.id}>{message.content}</div>)}
+
       </div>
       <div className='mt-4'>
-        <Form method='post'>
-          <input required type="text" name='author' className='border-2 border-green-300 focus:border-green-800 focus:border-3 rounded-md focus:outline-none p-1 mr-2' placeholder='Your name here' value={messageInput} onChange={(event) => setMessageInput(event.target.value)} />
-          <input required type="text" name='content' className='border-2 border-green-300 focus:border-green-800 focus:border-3 rounded-md focus:outline-none p-1 mr-2' placeholder='Say something...' value={messageInput} onChange={(event) => setMessageInput(event.target.value)} />
+        <Form method='post' ref={formRef}>
+          <input required type="text" name='author' className='border-2 border-green-300 focus:border-green-800 focus:border-3 rounded-md focus:outline-none p-1 mr-2' placeholder='Your name here' />
+          <input required type="text" name='content' className='border-2 border-green-300 focus:border-green-800 focus:border-3 rounded-md focus:outline-none p-1 mr-2' placeholder='Say something...' />
           <button type='submit' className='py-2 px-3 bg-green-800 text-gray-100 rounded-md disabled:bg-gray-400'>Send</button>
         </Form>
       </div>
